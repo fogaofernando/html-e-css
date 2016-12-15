70ad3434c2aae2da1a1daebef5c5f6753048cb17
fogaofernando <fogaofernando@gmail.com>
2016-12-15 17:59:07 -0200

    js

--- JavaScript/projeto/calcula-imc.js
+++ JavaScript/projeto/calcula-imc.js
@@ -1,11 +1,31 @@
-//calcula-imc.js
-var peso = 100;
-var altura = 0;
+//Instanciamento do Id dos elementos
+var trsPacientes = document.getElementsByClassName("paciente"); //Array de trs
+var cont = 0;   
 
-if(altura != 0){
-    var imc = peso / (altura * altura);
-    console.log(imc);
-} else{
-    console.log("Não posso executar uma divisão por 0!");
+for(cont = 0; cont <= trsPacientes.length -1;cont++ ){
+    var pacienteTr = trsPacientes[cont];
+    
+    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+    
+    var pacienteAtual = {nome:tdNome.textContent, 
+                         peso:tdPeso.textContent, 
+                         altura: tdAltura.textContent,
+                         imc:function(){
+                            
+                            if(this.altura <= 0 || this.peso <= 0){
+                                console.log("Erro: Não é possivel ter uma altura ou peso igual ou inferior a Zero...");
+                                imc = "Null";
+                            }else{
+                                var imc = this.peso/(this.altura * this.altura);
+                            }
+                             
+                            return imc;
+                         }};
+                         
+    
+    tdImc.textContent = pacienteAtual.imc();
+    console.log(tdImc.textContent);
 }
-
