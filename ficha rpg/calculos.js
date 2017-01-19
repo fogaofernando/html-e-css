da64bb49e57b43d1170313fd4d59306b3a8b89c1
fogaofernando <fogaofernando@gmail.com>
2017-01-19 12:50:32 -0200

    criação do locaStorage(top)

--- ficha rpg/calculos.js
+++ ficha rpg/calculos.js
@@ -1,2 +1,6 @@
-//Inputs
 
+alert(localStorage.getItem("lastname"));
+alert(localStorage.lastName);
+function salvar(){
+    localStorage.setItem("lastname",String(document.getElementById("nome").value));
+}
